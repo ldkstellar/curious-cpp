543b25d338213a2700d971e6b553c2ebc671986f
leedongkyu <leedongkyu0225@gmail.com>
2025-12-30 19:16:36 +0900

    feat: virtual class

--- class/virtual_inheritance.cpp
+++ class/virtual_inheritance.cpp
@@ -7,13 +7,13 @@ class Base {
   Base() { cout << "Base 생성자 호출" << endl; }
 };
 
-// 중간 클래스 1 (가상 상속 O)
+// 중간 클래스 1 (가상 상속 O) base 개체를 한개만 만들때 사용한다. 이둘을 안해주면 base가 두개 생긴다.
 class Intermediate1 : virtual public Base {
  public:
   Intermediate1() { cout << "Intermediate1 생성자 호출" << endl; }
 };
 
-// 중간 클래스 2 (가상 상속)
+// 중간 클래스 2 (가상 상속) 이하 동문
 class Intermediate2 : virtual public Base {
  public:
   Intermediate2() { cout << "Intermediate2 생성자 호출" << endl; }
@@ -29,3 +29,5 @@ int main() {
   FinalDerived obj;
   return 0;
 }
+
+///1. base 생성자 호출 2. 가상 생성자 호출  3.비가상생성자 호출 
